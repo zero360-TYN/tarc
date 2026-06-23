da822a8d9c73d8ea099d9acf441fded4984b04d9
ZERO <yuxuan674@gmail.com>
2026-06-23 23:16:04 +0800

    zip

--- AMIT2014/P1/Demo/wwwroot/js/app.js
+++
@@ -1,6 +0,0 @@
-﻿// Initiate GET request (AJAX-supported)
-$(document).on('click', '[data-get]', e => {
-    e.preventDefault();
-    const url = e.target.dataset.get;
-    location = url || location;
-});
